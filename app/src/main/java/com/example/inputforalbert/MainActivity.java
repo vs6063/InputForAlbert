3f4a607cf4eb9599bc3bed376d5c01f001d9124a
Victor Shen <v.shen@student.unsw.edu.au>
2017-09-12 23:12:05 +1000

    added left right down swipe functionality

--- app/src/main/java/com/example/inputforalbert/MainActivity.java
+++ app/src/main/java/com/example/inputforalbert/MainActivity.java
@@ -6,12 +6,16 @@ import android.view.MotionEvent;
 import android.widget.TextView;
 import android.view.GestureDetector;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
     private static int counter = 0;
     private static int lastCount = 0;
     private TextView textView;
     private GestureDetector gestureStuff;
+    private ArrayList<TextView> pin;
+    private int currentPinPos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +23,27 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.textView);
         this.gestureStuff = new GestureDetector(this,this);
-
+        pin = new ArrayList<TextView>();
+        pin.add((TextView) findViewById(R.id.digit1));
+        pin.add((TextView) findViewById(R.id.digit2));
+        pin.add((TextView) findViewById(R.id.digit3));
+        pin.add((TextView) findViewById(R.id.digit4));
+        for (int i = 0; i < 4; i++) {
+            pin.get(i).setText("*");
+        }
+        currentPinPos = 0;
     }
 
     @Override
     public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        if(v > 10000)
+            setDigit("RIGHT");
+        else if(v < -10000)
+            setDigit("LEFT");
+        else if(v1 > 10000)
+            setDigit("DOWN");
+        if(v1 < -10000)
+            textView.setText(String.valueOf("UP"));
         return true;
     }
 
@@ -37,7 +57,7 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
                 counter++;
             }
             if (counter > 9) {
-                counter = 0;
+                counter = 9;
             }
             textView.setText(String.valueOf(counter));
 
@@ -58,17 +78,33 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
 
     @Override
     public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-        if(v > 100)
-            textView.setText(String.valueOf("LEFT"));
-        else if(v < -100)
-            textView.setText(String.valueOf("RIGHT"));
-        else if(v1 > 100)
-            textView.setText(String.valueOf("UP"));
-        if(v1 < -100)
-            textView.setText(String.valueOf("DOWN"));
+//        if(v > 100)
+//            setDigit("LEFT");
+//        else if(v < -100)
+//            setDigit("RIGHT");
+//        else if(v1 > 100)
+//            textView.setText(String.valueOf("UP"));
+//        if(v1 < -100)
+//            setDigit("DOWN");
         return true;
     }
 
+    private void setDigit(String swipe) {
+        if(swipe.equals("RIGHT")) {
+            if(currentPinPos == 4) return;
+            pin.get(currentPinPos).setText(String.valueOf(counter));
+            counter = 0;
+            textView.setText("0");
+            currentPinPos++;
+        } else if(swipe.equals("LEFT")){
+            counter = 0;
+        } else if(swipe.equals("DOWN")){
+            for (int i = 0; i < 4; i++) {
+                pin.get(i).setText("*");
+            }
+            currentPinPos = counter = 0;
+        }
+    }
     @Override
     public void onLongPress(MotionEvent motionEvent) {
 
