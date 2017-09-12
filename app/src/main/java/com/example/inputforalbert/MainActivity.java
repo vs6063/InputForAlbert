15384bfe048caa690851394351d200a7f608759f
will-weng <william.weng@hotmail.com>
2017-09-12 22:06:10 +1000

    gestures alpha

--- app/src/main/java/com/example/inputforalbert/MainActivity.java
+++ app/src/main/java/com/example/inputforalbert/MainActivity.java
@@ -4,34 +4,79 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.widget.TextView;
+import android.view.GestureDetector;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
-    public static int counter = 0;
-    public static int lastCount = 0;
+    private static int counter = 0;
+    private static int lastCount = 0;
+    private TextView textView;
+    private GestureDetector gestureStuff;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        this.gestureStuff = new GestureDetector(this,this);
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return true;
     }
 
     @Override
     public boolean onTouchEvent (MotionEvent e) {
-        if (e.getPointerCount() < lastCount) {
-            counter += (lastCount - e.getPointerCount());
-        }
-        if (e.getAction() == MotionEvent.ACTION_UP) {
-            counter++;
-        }
-        if (counter > 9) {
-            counter = 0;
-        }
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(String.valueOf(counter));
+        if(!this.gestureStuff.onTouchEvent(e)) {
+            if (e.getPointerCount() < lastCount) {
+                counter += (lastCount - e.getPointerCount());
+            }
+            if (e.getAction() == MotionEvent.ACTION_UP) {
+                counter++;
+            }
+            if (counter > 9) {
+                counter = 0;
+            }
+            textView.setText(String.valueOf(counter));
 
-        lastCount = e.getPointerCount();
-
+            lastCount = e.getPointerCount();
+        }
         return true;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        if(v > 100)
+            textView.setText(String.valueOf("LEFT"));
+        else if(v < -100)
+            textView.setText(String.valueOf("RIGHT"));
+        else if(v1 > 100)
+            textView.setText(String.valueOf("UP"));
+        if(v1 < -100)
+            textView.setText(String.valueOf("DOWN"));
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
     }
 
     /*
