b5eed91224151fb8145e6f35554c846a03e53b1c
Victor Shen <v.shen@student.unsw.edu.au>
2017-09-12 21:12:22 +1000

    FIXED MULTI-TOUCH!!!!

--- app/src/main/java/com/example/inputforalbert/MainActivity.java
+++ app/src/main/java/com/example/inputforalbert/MainActivity.java
@@ -8,11 +8,32 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     public static int counter = 0;
+    public static int lastCount = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onTouchEvent (MotionEvent e) {
+        if (e.getPointerCount() < lastCount) {
+            counter += (lastCount - e.getPointerCount());
+        }
+        if (e.getAction() == MotionEvent.ACTION_UP) {
+            counter++;
+        }
+        if (counter > 9) {
+            counter = 0;
+        }
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(String.valueOf(counter));
+
+        lastCount = e.getPointerCount();
+
+        return true;
+    }
+
     /*
     @Override
     public boolean onTouchEvent(MotionEvent e) {
@@ -45,12 +66,4 @@ public class MainActivity extends AppCompatActivity {
         return trigger;
     }
     */
-    @Override
-    public boolean onTouchEvent (MotionEvent e) {
-        counter += e.getPointerCount();
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(String.valueOf(counter));
-        return true;
-    }
-
 }
